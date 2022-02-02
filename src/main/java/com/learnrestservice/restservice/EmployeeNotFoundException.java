26ecc53c34e94a2b984feef96ebb4ece2e0c0a06
pilpod <giacomo.corso@gmail.com>
2022-02-02 14:52:00 +0100

    DeleteMapping added in controller

---
+++ src/main/java/com/learnrestservice/restservice/EmployeeNotFoundException.java
@@ -0,0 +1,9 @@
+package com.learnrestservice.restservice;
+
+public class EmployeeNotFoundException extends RuntimeException {
+
+    public EmployeeNotFoundException(Long id) {
+        super("Could not find employee " + id);
+    }
+
+}
--- src/main/java/com/learnrestservice/restservice/controllers/EmployeeController.java
+++ src/main/java/com/learnrestservice/restservice/controllers/EmployeeController.java
@@ -2,9 +2,13 @@ package com.learnrestservice.restservice.controllers;
 
 import java.util.List;
 
+import com.learnrestservice.restservice.EmployeeNotFoundException;
 import com.learnrestservice.restservice.contracts.EmployeeRepository;
 import com.learnrestservice.restservice.models.Employee;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -29,8 +33,18 @@ public class EmployeeController {
     public Employee store(@RequestBody Employee newEmployee) {
         return repository.save(newEmployee);
     }
-    
 
+    @GetMapping("/employees/{id}")
+    public Employee show(@PathVariable Long id) {
+        return repository.findById(id).orElseThrow(()-> new EmployeeNotFoundException(id));
+    }
+    
+    @DeleteMapping("/employees/{id}")
+    public Employee destroy(@PathVariable Long id) {
+        Employee employee = repository.findById(id).orElseThrow(()-> new EmployeeNotFoundException(id));
+        repository.deleteById(id);
+        return employee;
+    }
 
 
     
