52fcdb7795712264043812c996790f36105a5354
pilpod <giacomo.corso@gmail.com>
2022-02-02 13:43:31 +0100

    added PostMapping /employees

--- src/main/java/com/learnrestservice/restservice/LoadDatabase.java
+++ src/main/java/com/learnrestservice/restservice/LoadDatabase.java
@@ -17,7 +17,7 @@ public class LoadDatabase {
     CommandLineRunner initDatabase(EmployeeRepository repository)
     {
         return args -> {
-            log.info("Preloading " + repository.save(new Employee("Bilbo Baggins", "burglar")));
+            log.info("Preloading " + repository.save(new Employee("Bilbo Bolson", "burglar")));
             log.info("Preloading " + repository.save(new Employee("Frodo Baggins", "thief")));
           };
     }    
--- src/main/java/com/learnrestservice/restservice/controllers/EmployeeController.java
+++ src/main/java/com/learnrestservice/restservice/controllers/EmployeeController.java
@@ -6,6 +6,9 @@ import com.learnrestservice.restservice.contracts.EmployeeRepository;
 import com.learnrestservice.restservice.models.Employee;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
 public class EmployeeController {
@@ -21,6 +24,12 @@ public class EmployeeController {
     {
         return repository.findAll();
     }
+
+    @PostMapping("/employees")
+    public Employee store(@RequestBody Employee newEmployee) {
+        return repository.save(newEmployee);
+    }
+    
 
 
 
