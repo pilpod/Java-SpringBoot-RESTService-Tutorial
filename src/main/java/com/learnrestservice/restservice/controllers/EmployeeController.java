29a5e22fc1a88bc19f9d2284565fe84601debb5f
pilpod <giacomo.corso@gmail.com>
2022-02-02 19:39:27 +0100

    added PutMapping

--- src/main/java/com/learnrestservice/restservice/controllers/EmployeeController.java
+++ src/main/java/com/learnrestservice/restservice/controllers/EmployeeController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 
@@ -44,6 +45,19 @@ public class EmployeeController {
         Employee employee = repository.findById(id).orElseThrow(()-> new EmployeeNotFoundException(id));
         repository.deleteById(id);
         return employee;
+    }
+
+    @PutMapping("/employees/{id}")
+    public Employee update(@RequestBody Employee newEmployee, @PathVariable Long id) {
+        return repository.findById(id).map(employee -> {
+            employee.setName(newEmployee.getName());
+            employee.setRole(newEmployee.getRole());
+            return repository.save(employee);
+        })
+        .orElseGet(() -> {
+            newEmployee.setId(id);
+            return repository.save(newEmployee);
+        });
     }
 
 
